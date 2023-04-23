cee7ec4fb04b58d04cf2b6f1b5bd614e1ec4ca86
jbride <jbride2001@yahoo.com>
2023-04-23 14:30:46 -0600

    latest

--- print_test.cpp
+++ print_test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <array>
 
 int main() {
-    std::cout << "hello";
+    int intVar = 10;
+    double doubleVar0 = 3.3;
+    double doubleVar1 = 5.0;
+    std::array<double,2> myArray = {doubleVar0,doubleVar1};
+    //myArray.fill(2.0);
+    std::cout << "hello\t" << (double)(myArray[0] / myArray[1]) << "\n";
     return 0;
 }
