cc17c0146c213e2c6f7431c984b4e6f529d1e35a
jbride <jbride2001@yahoo.com>
2023-05-07 09:04:23 -0600

    functions and threads

--- nullptr.cpp
+++ nullptr.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
     int *p = nullptr;
 
+    // Define a reference to a nullptr
+    int& r = *p;
+
     // Force a segfault
     // Segfaults are caused by a program trying to read or write an illegal memory location
     // Note that where the segfault occurs is generally only a clue as to where "the mistake which causes" it is in the code.
     // An important clue, but it's not necessarily where the problem resides
-    std::cout << "deferenced pointer p = " << *p << endl;
+    std::cout << "deferenced pointer p = " << r << endl;
 
     return 0;
 }
--- pointers.cpp
+++ pointers.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
-#include <array>
 using namespace std;
 
 class ArrayTest {
 
-
-    int fiveA[5] = {1,2,3,4,5};
-    int *p1 = nullptr;
+    int globalIntArray[5] = {1,2,3,4,5};
+    int* pA = nullptr;
 
     public:
 
         // Arrays and pointers are almost indistinguishable from each other in C++.
         // A pointer to the beginning of an array, the address of the first element & the bare array name all mean the same thing
         void testArray() {
-            p1 = &fiveA[2];
-            std::cout << "\n" << &fiveA << "\t" << p1 << endl;
 
-            std::cout << "\n" << fiveA[4] << "\t" << p1[2] << endl;
+            // C++ 17 provides std::size()
+            std::cout << "testArray() array size = " << std::size(globalIntArray) << endl;
+
+            // set pointer to address-of 3rd element of array
+            pA = &globalIntArray[2];
+            std::cout << "testArray() " << &globalIntArray << "\t" << pA << endl;
+
+            // The following are equivalent expressions
+            // int fifthE = pA[2];
+            int fifthE = *(pA + 2);
+
+            // print the value 2 elements beyond the pointer currently points to
+            std::cout << "testArray(): " << globalIntArray[4] << "\t" << fifthE << "\t" << pA << endl;
         }
-
 };
 
 int main() {
+    std:cout << "main() size of int = " << sizeof(int) << endl;
+
     int i = 12345;
 
     // create a pointer to the address of the variable
-    int *pI = &i;
+    int* pI = &i;
 
-    std::cout << "pI = " << pI << ", &i = " << &i << endl;
+    std::cout << "main() pI = " << pI << ", &i = " << &i << endl;
 
     // add 3 to the de-referenced pointer (the actual variable, i)
     int j = *pI + 3;
 
-    // increment the variable i to 2
+    // increment the variable i by 2
     *pI = *pI +2;
 
-    std::cout <<  "i = " << i << ", pI = " << pI << endl;
+    std::cout <<  "main() i = " << i << ", pI = " << pI << ", j = " << j << endl;
 
     ArrayTest aTest;
     aTest.testArray();
