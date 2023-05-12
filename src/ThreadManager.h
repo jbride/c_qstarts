135d2628607d90b27a7f24d49c15f4127b5c0faf
jbride <jbride2001@yahoo.com>
2023-05-12 12:26:10 -0600

    ThreadManager

---
+++ src/ThreadManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CommonHeaders.h"
+
+// Utility that associates a name with a thread for logging purposes
+class ThreadManager {
+public:
+	// Future: Implement a CreateThread method that takes the name, function, and parameters.
+	static std::string GetCurrentThreadName();
+	static std::string GetThreadName(const std::thread::id& threadId);
+	static void SetThreadName(const std::thread::id& threadId, const std::string& threadName);
+	static void SetCurrentThreadName(const std::string& threadName);
+
+private:
+	inline static std::map<std::thread::id, std::string> m_threadNamesById{ };
+	inline static std::mutex m_MapLock;
+};
--- src/threads.cpp
+++ src/threads.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <thread>
 #include <random>
+#include "ThreadManager.h"
 
 using namespace std;
 
@@ -11,7 +12,7 @@ inline void printTime(int loops, int delayMillis) {
     for(int x = 0; x<loops; x++){
         auto now = std::chrono::system_clock::now();
 
-        std::cout << std::this_thread::get_id() << "\t" << x << "\t: printTime() millis since Unix Epoch (00:00:00 UTC on 1 January 1970) " << now.time_since_epoch().count() << endl;
+        std::cout << ThreadManager::GetCurrentThreadName() << "\t" << x << "\t: printTime() millis since Unix Epoch (00:00:00 UTC on 1 January 1970) " << now.time_since_epoch().count() << endl;
 
         auto stime = std::chrono::system_clock::to_time_t(now);
 
@@ -26,7 +27,7 @@ inline void printTime(int loops, int delayMillis) {
 void printString(int loops, int delayMillis, string stringArg) {
 
     for(int x = 0; x<loops; x++){
-        std::cout << std::this_thread::get_id() << "\t" << x << "\t: printString() " << stringArg << endl;
+        std::cout << ThreadManager::GetCurrentThreadName() << "\t" << x << "\t: printString() " << stringArg << endl;
         std::mt19937_64 eng{std::random_device{}()};  // or seed however you want
         std::uniform_int_distribution<> dist{0, delayMillis};
         std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
@@ -35,17 +36,22 @@ void printString(int loops, int delayMillis, string stringArg) {
 }
 
 int main() {
-    std:: cout << std::this_thread::get_id() << "\t\t : starting" << endl << endl;
+    ThreadManager::SetCurrentThreadName("MAIN");
+    std:: cout << ThreadManager::GetCurrentThreadName() << "\t\t : starting" << endl << endl;
+
     std::thread tTime(printTime, 20, 500);
+    ThreadManager::SetThreadName(tTime.get_id(), "TIME_THREAD");
+
     // detch() :  thread will continue its execution until it finishes w/out being managed by the std::thread object, which will no longer own any thread
     // NOTE:  this thread will get automatically killed if not completed by the time the main thread terminates
     tTime.detach();
 
     std::thread tString(printString, 10, 200, "azra");
+    ThreadManager::SetThreadName(tString.get_id(), "STRING_THREAD");
     // join() : block main thread until tString thread is finished
     tString.join();
 
-    std::cout << std::this_thread::get_id() << " : main() about to sleep" << endl;
+    std::cout << ThreadManager::GetCurrentThreadName() << " : main() about to sleep" << endl;
     std::chrono::seconds sleepTime = 2s;
     std::this_thread::sleep_for(sleepTime);
 
